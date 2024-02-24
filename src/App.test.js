c16eb55a855db2b7b7827a0943ea529f569a3535
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2024-02-24 13:54:52 -0800

    test code updated.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import '@testing-library/jest-dom';
-
 test('renders GetToDoItem component', () => {
     render(<App />);
     const linkElement = screen.getByText('My To Do Items');
